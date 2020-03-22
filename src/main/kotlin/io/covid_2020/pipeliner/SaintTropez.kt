223d37f9ae0ef79ecb7b75a3e926559ba5ae1adb
smatthewenglish <s.matthew.english@gmail.com>
2020-03-22 16:03:29 -0400

    got em! ;-)

--- src/main/kotlin/io/covid_2020/pipeliner/SaintTropez.kt
+++ src/main/kotlin/io/covid_2020/pipeliner/SaintTropez.kt
@@ -1,21 +1,45 @@
 package io.covid_2020.pipeliner
 
-class SaintTropez {
-
-    private val address: String = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=csbs"
-
-    fun activity(player_id: String, route: String) {
-        try {
-            khttp.post(
-                url = "${this.address}write?db=tschess",
-                data = "activity player=\"${player_id}\",route=\"${route}\""
-            )
-        } catch (e: Exception) {
-            print(e.localizedMessage)
-        }
-    }
-}
+import khttp.responses.Response
+import org.json.JSONArray
+import org.json.JSONObject
 
 fun main() {
-    println("Hello Kotlin!")
+    val stateMap: MutableMap<String,Info> = mutableMapOf()
+
+    val address: String = "https://coronavirus-tracker-api.herokuapp.com/v2/locations?source=csbs"
+    val response: Response = khttp.get(url = address)
+    val obj: JSONObject = response.jsonObject
+
+    val locations: JSONArray = obj["locations"] as JSONArray
+    locations.forEach() {
+        val place = it as JSONObject
+        val state = place.get("state") as String
+        val latest: JSONObject = place.get("latest") as JSONObject
+
+        var confirmed: Int = latest.getInt("confirmed")
+        var deaths: Int = latest.getInt("deaths")
+        var recovered: Int = latest.getInt("recovered")
+
+        if(stateMap.containsKey(state)) {
+            confirmed += stateMap[state]!!.confirmed
+            deaths += stateMap[state]!!.deaths
+            recovered += stateMap[state]!!.recovered
+        }
+        stateMap[state] = Info(confirmed, deaths, recovered)
+    }
+
+    stateMap.forEach() {
+        println("${it.key}\n" +
+                "confirmed: ${it.value.confirmed}\n" +
+                "deaths: ${it.value.deaths}\n" +
+                "recovered: ${it.value.recovered}\n\n")
+    }
+
 }
+
+data class Info(
+    val confirmed: Int,
+    val deaths: Int,
+    val recovered: Int
+)
